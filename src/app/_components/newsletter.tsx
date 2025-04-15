e32cd01d875eefb36f92f371e77d7fe3a2c2bbdc
conuaz <lucas.olasz@tty2000.com.br>
2025-04-15 15:02:14 -0300

    Estilização do componente newsletter

--- src/app/_components/newsletter.tsx
+++ src/app/_components/newsletter.tsx
@@ -1,7 +1,30 @@
+import ButtonHero from "./buttonHero";
+
 export default function Newsletter() {
   return (
-    <div>
-      <h1>oi</h1>
+    <div className="w-full py-16 px-4 text-white">
+      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+        <div className="lg:col-span-2 my-4">
+          <h1 className="md:text-3xl sm:text-2xl text-xl font-bold py-2">
+            Want tips & tricks to optimize your flow?
+          </h1>
+          <p>Sign up to our newsletter and stay up to date</p>
+        </div>
+        <div className="my-4">
+          <div className="flex flex-col sm:flex-row items-center justify-between w-full">
+            <input
+              className="bg-white text-gray-400 p-3 rounded-md mr-4 w-full"
+              type="email"
+              placeholder="Enter your email"
+            />
+            <ButtonHero>Notify me</ButtonHero>
+          </div>
+          <p>
+            We care bout the protection of your data. Read our{" "}
+            <span className="text-[#00df9a]">Privacy Policy</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
