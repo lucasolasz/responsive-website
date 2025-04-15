b6fc008dac4271e38f08b594e82606d7765d2217
conuaz <lucas.olasz@tty2000.com.br>
2025-04-15 13:58:19 -0300

    Finalização do componente hero

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,5 +1,38 @@
-import { DiVim } from "react-icons/di";
+"use client";
+import { ReactTyped } from "react-typed";
+import ButtonHero from "./buttonHero";
 
 export function Hero() {
-  return <div>Ola</div>;
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 mb-1">
+          Grow with data.
+        </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+            Fast, flexible, financing for
+          </p>
+
+          <ReactTyped
+            className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+            strings={["BTB", "BTC", "SASS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+        </div>
+
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your data analytics to increase revenue for BTB, BTC, & SASS
+          platforms.
+        </p>
+
+        <ButtonHero>Get Started</ButtonHero>
+      </div>
+    </div>
+  );
 }
