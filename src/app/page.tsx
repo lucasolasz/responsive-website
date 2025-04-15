707f3db80b66177303f57ef61ee434fc0be99ab2
conuaz <lucas.olasz@tty2000.com.br>
2025-04-15 16:22:18 -0300

    Inclusão do componente footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Products from "./_components/products";
 import { Hero } from "./_components/hero";
 import { NavBar } from "./_components/navbar";
 import Newsletter from "./_components/newsletter";
+import Footer from "./_components/footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Analytics />
       <Newsletter />
       <Products />
+      <Footer />
     </div>
   );
 }
