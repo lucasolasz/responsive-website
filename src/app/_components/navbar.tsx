178b704e2b74165cf58a7e09d63b5c0e36f8f6fa
conuaz <lucas.olasz@tty2000.com.br>
2025-04-15 17:22:06 -0300

    Corrige label

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -16,7 +16,7 @@ export function NavBar() {
       <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
-        <li className="p-4">Rerources</li>
+        <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
@@ -46,7 +46,7 @@ export function NavBar() {
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
           <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Rerources</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
           <li className="p-4 border-b border-gray-600">About</li>
           <li className="p-4">Contact</li>
         </ul>
