ea83cfd571a3ed94290423d888b39f5b9e064984
conuaz <lucas.olasz@tty2000.com.br>
2025-04-15 16:25:28 -0300

    Classes iniciais

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,6 +1,14 @@
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <div>
+    <div className="max-w-[1240px] max-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
       <h1>footer</h1>
     </div>
   );
