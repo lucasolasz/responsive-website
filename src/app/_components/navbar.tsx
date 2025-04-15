b00816e4cc4f843f23872cb906f7fa3a78e5f2f9
conuaz <lucas.olasz@tty2000.com.br>
2025-04-15 11:58:35 -0300

    Centralizou o nav em telas maiores que 1240px

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,14 @@
+export function NavBar() {
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <ul className="flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Rerources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+    </div>
+  );
+}
