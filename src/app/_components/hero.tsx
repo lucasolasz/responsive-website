5fdf5fe4745e8f39f7dd65b4d00d9a91138ef81f
conuaz <lucas.olasz@tty2000.com.br>
2025-04-15 12:26:14 -0300

    Inclusão do componente hero

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,5 @@
+import { DiVim } from "react-icons/di";
+
+export function Hero() {
+  return <div>Ola</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Hero } from "./_components/hero";
 import { NavBar } from "./_components/navbar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 }
